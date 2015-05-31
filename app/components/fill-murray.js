91a4004bb7c6a04043e00d0b60aae00ed4d144ec
Chad Ostrowski <hi@chadoh.com>
2015-05-31 10:55:05 -0400

    Try doing what the book says

--- app/components/fill-murray.js
+++ app/components/fill-murray.js
@@ -1,1 +1,15 @@
-export { default } from 'ember-cli-fill-murray/components/fill-murray';
+// import fillMurray from 'ember-cli-fill-murray/components/fill-murray';
+// export { default } from 'ember-cli-fill-murray/components/fill-murray';
+//
+import Ember from 'ember';
+// import layout from '../templates/components/fill-murray';
+
+export default Ember.Component.extend({
+  // layout: layout,
+  height: 100,
+  width: 100,
+  src: Ember.computed('height', 'width', function() {
+    var base = 'http://www.fillmurray.com/';
+    return base + this.get('width') +  '/' + this.get('height');
+  })
+});
