8900dc4276c8ccc7f2181aa9a385f5498602157d
Chad Ostrowski <hi@chadoh.com>
2015-05-31 11:00:07 -0400

    Remove kruft - it worked!

--- app/components/fill-murray.js
+++ app/components/fill-murray.js
@@ -1,11 +1,6 @@
-// import fillMurray from 'ember-cli-fill-murray/components/fill-murray';
-// export { default } from 'ember-cli-fill-murray/components/fill-murray';
-//
 import Ember from 'ember';
-// import layout from '../templates/components/fill-murray';
 
 export default Ember.Component.extend({
-  // layout: layout,
   height: 100,
   width: 100,
   src: Ember.computed('height', 'width', function() {
